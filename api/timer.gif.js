c7b4953c494fbc42748776d3d63a7f04f5f3e3eb
Gabriel Pires <gabrielpires@gabrielpires.local>
2026-06-26 13:58:58 -0400

    fix: remove arcTo usage not supported by pureimage

--- api/timer.gif.js
+++ api/timer.gif.js
@@ -75,14 +75,7 @@ function toRemainingParts(totalMs) {
 
 function roundedRect(ctx, x, y, width, height, radius, fillStyle) {
   ctx.fillStyle = fillStyle;
-  ctx.beginPath();
-  ctx.moveTo(x + radius, y);
-  ctx.arcTo(x + width, y, x + width, y + height, radius);
-  ctx.arcTo(x + width, y + height, x, y + height, radius);
-  ctx.arcTo(x, y + height, x, y, radius);
-  ctx.arcTo(x, y, x + width, y, radius);
-  ctx.closePath();
-  ctx.fill();
+  ctx.fillRect(x, y, width, height);
 }
 
 function drawTile(ctx, label, value, x, y) {
